a8bb9052772e1991fc155711b323b19563d5521e
Wellington dos Santos <w_santos1993@outlook.com>
2022-10-17 19:41:39 -0300

    Criacao do header, footer e padronização do site

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,85 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link>
-    </nav>
-    <router-view/>
+    <Header/>
+    <main id="main">
+      <router-view/>
+    </main>
+    <Footer/>
   </div>
 </template>
 
-<style>
+<script>
+import Header from './components/base/header/Header.vue';
+import Footer from './components/base/footer/Footer.vue';
 
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+body, ul, h1, h2, li, p {
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url('./assets/img/pattern.svg') repeat top;
+}
+
+a {
+  color: #345;
+  list-style: none;
+  text-decoration: none;
+
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1ren;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  cursor: pointer;
+
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+}
+
+#main {
+  flex: 1;
+}
 </style>
---
+++ src/components/base/header/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/" class="logo">
+                <img src="@/assets/img/ranek.svg" alt="logoHeader">
+            </router-link>
+            <router-link class="btn" to="/login">Vender/ login</router-link>
+        </nav>
+    </header>
+</template>
+
+<script>
+import HeaderComponent from './HeaderCoimponents';
+export default HeaderComponent;
+</script>
+
+<style lang="scss" scoped>
+@import'./Header.scss';
+</style>
