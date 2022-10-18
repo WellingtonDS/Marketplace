0c8a2ee64dbbe17441e91c27a1a5788044f77ee8
Wellington dos Santos <w_santos1993@outlook.com>
2022-10-18 12:04:57 -0300

    estilizacao lista de produtos e mensagem de erro

--- src/components/productList/ProductList.vue
+++ src/components/productList/ProductList.vue
@@ -1,10 +1,17 @@
 <template>      
-    <section class="product-container">
-        <div v-for="product in products" :key="product.id" name="product">
-            <img v-if="product.photos" src="produtct.photos[0].src" alt="product.photos[0].title">
-            <p class="price">{{product.price}}</p>
-            <h2 class="title">{{product.name}}</h2>
-            <p class="description"> {{product.desciption}}</p>
+    <section class="products-container">
+        <div v-if="products && products.length" class="products">
+            <div class="product" v-for="product in products" :key="product.id" name="product">
+                <router-link to="/">
+                    <img v-if="product.photos" src="produtct.photos[0].src" alt="product.photos[0].title">
+                    <p class="price">{{product.price}}</p>
+                    <h2 class="title">{{product.name}}</h2>
+                    <p class="description"> {{product.desciption}}</p>
+                </router-link>
+            </div>
+        </div>
+        <div v-else-if="products && products.length === 0">
+            <p class="no-result">Busca sem resultado. Tente buscar por outro termo.</p>
         </div>
     </section>
 </template>
