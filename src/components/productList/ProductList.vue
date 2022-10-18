1ca71146cac55b7046e2f02ae6ff8b7830e6fe23
Wellington dos Santos <w_santos1993@outlook.com>
2022-10-17 21:51:55 -0300

    criacao de arquivo para adiministrar todos os metodos do axios

--- src/components/productList/ProductList.vue
+++ src/components/productList/ProductList.vue
@@ -1,5 +1,13 @@
 <template>      
-    <h1>list</h1>
+    <section class="product-container">
+        <div v-for="product in products" :key="product.id" name="product">
+            <img v-if="product.photos" src="produtct.photos[0].src" alt="product.photos[0].title">
+            <p class="price">{{product.price}}</p>
+            <h2 class="title">{{product.name}}</h2>
+            <p class="description"> {{product.desciption}}</p>
+
+        </div>
+    </section>
 </template>
 
 <script lang="js">
