7e2dbf724079cfb433cb91fe8012b3e7e94e04e5
Wellington dos Santos <w_santos1993@outlook.com>
2022-10-26 13:51:52 -0300

    criacao do metodo para atualizar dados do usuario

--- src/service/service.js
+++ src/service/service.js
@@ -13,13 +13,13 @@ export const api = {
         return axiosInstance.post(endpoint, body);
     },
 
+    put(endpoint, body) {
+        return axiosInstance.put(endpoint, body);
+    },
+
     delete(endpoint) {
         return axiosInstance.delete(endpoint);
     },
-
-    put(endpoint) {
-        return axiosInstance.put(endpoint);
-    }
 };
 
 
