e32a6f7caa248b552040f27e24de0e9b9438ce51
Wellington dos Santos <w_santos1993@outlook.com>
2022-10-26 13:33:34 -0300

    criacao de delete para remover os produtos

--- src/service/service.js
+++ src/service/service.js
@@ -11,6 +11,14 @@ export const api = {
 
     post(endpoint, body) {
         return axiosInstance.post(endpoint, body);
+    },
+
+    delete(endpoint) {
+        return axiosInstance.delete(endpoint);
+    },
+
+    put(endpoint) {
+        return axiosInstance.put(endpoint);
     }
 };
 
--- src/views/user/ProductsUser.vue
+++ src/views/user/ProductsUser.vue
@@ -7,6 +7,7 @@
             <li v-for="(product, index ) in product_user" :key="index">
                 <ProductItem :product="product">
                     <p>{{product.description}}</p>
+                    <button class="delete" @click.prevent="deletarProduto(product.id)"></button>
                 </ProductItem>    
             </li>
         </transition-group>
@@ -19,6 +20,7 @@
 import ProductItem from "@/components/productItem/ProductItem.vue"
 import {mapState, mapActions } from 'vuex';
 import CreateProduct from '@/components/createProduct/CreateProduct.vue';
+import { api } from "@/service/service";
 export default {
     name: "ProductUser",
     components: {
@@ -30,7 +32,17 @@ export default {
         ...mapState(["login","user","product_user"])
     },
     methods: {
-        ...mapActions(["getProductUser"])
+        ...mapActions(["getProductUser"]),
+        deletarProduto(id) {
+        const confirmar = window.confirm("Deseja remover este produto?");
+        if (confirmar) {
+            api.delete(`/product/${id}`).then(() => {
+                this.getProductUser();
+            }).catch(error => {
+                console.log(error.response)
+            });   
+        }
+        }
     },
 
     watch: {
@@ -54,6 +66,18 @@ h2 {
     margin-bottom: 20px;   
 }
 
+.delete {
+   position: absolute;
+   top: 0;
+   right: 0;
+   background: url("../../assets/img/remove.svg") no-repeat center center;
+   width: 24px;
+   height: 24px;
+   text-indent: -140px;
+   cursor: pointer;
+   overflow: hidden;
+   border: none;
+}
 .list-enter, .list-leave-to {
     opacity: 0;
     transform: translate3d(20px, 0, 0);
